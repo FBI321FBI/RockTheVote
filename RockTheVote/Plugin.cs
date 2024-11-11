9b05cda192523d0a5abed3cb832ec27a2e1c5733
Ilya Post <im-popow13@mail.ru>
2024-11-11 19:25:07 +0500

    Добавьте файлы проекта.

---
+++ RockTheVote/Plugin.cs
@@ -0,0 +1,13 @@
+﻿using CounterStrikeSharp.API.Core;
+
+namespace RockTheVote
+{
+	public class Plugin : BasePlugin
+	{
+		#region Properties
+		public override string ModuleName => throw new NotImplementedException();
+
+		public override string ModuleVersion => throw new NotImplementedException();
+		#endregion
+	}
+}
